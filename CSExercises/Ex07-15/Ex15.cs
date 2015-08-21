6d7703889dfc4d497926bea2d3755fb3cd2d322f
thiri91 <e001340@u.nus.edu>
2015-08-21 23:19:15 +0630

    no 15 rechanged

--- CSExercises/Ex07-15/Ex15.cs
+++ CSExercises/Ex07-15/Ex15.cs
@@ -12,15 +12,23 @@ namespace CSExercises
             Console.WriteLine("Enter x: ");			
             double x= Convert.ToDouble(Console.ReadLine());
 			
-			Console.WriteLine("Enter y: ");
-            double y = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter y: ");
+            //double y = Convert.ToDouble(Console.ReadLine());
 
-			Console.WriteLine("Enter z: ");
-            double z = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter z: ");
+            //double z = Convert.ToDouble(Console.ReadLine());
+            int one_digit,two_digit,three_digit;//153
+            one_digit = (int)x / 100;
+            int remain =(int) x % 100;
+            two_digit=(int)remain/10;
+            three_digit=(int)remain%10;
 
-            string input=x+""+y+""+z;
-            double armstrong = Math.Pow(x, 3) + Math.Pow(y, 3) + Math.Pow(z, 3);
-            if (Convert.ToDouble(input) == armstrong)
+          
+
+          //  string input=x+""+y+""+z;
+            double armstrong = Math.Pow(one_digit, 3) + Math.Pow(two_digit, 3) + Math.Pow(three_digit, 3);
+            Console.WriteLine(armstrong);
+            if (x == armstrong)
                 Console.WriteLine("armstrong number");
             else Console.WriteLine("not armstrong number");
         }
